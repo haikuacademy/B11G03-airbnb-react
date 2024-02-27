662c6a62f539f4ab964e8cc4bdb364ddda402beb
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-27 11:48:44 +0700

    Signup component and route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Houses from './components/Houses'
 import House from './components/House'
+import Signup from './components/Signup'
 
 function Router() {
     return (
@@ -9,6 +10,7 @@ function Router() {
             <Routes>
                 <Route path='/' element={<Houses />} />
                 <Route path='/houses/:id' element={<House />} />
+                <Route path='/signup' element={<Signup />} />
             </Routes>
         </BrowserRouter>
     )
