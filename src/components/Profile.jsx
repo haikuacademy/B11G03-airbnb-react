acc9df2bc73eb0bdd7309c8b297647982c77f87a
I-bangash <irtezabangash3d@gmail.com>
2024-02-27 14:32:02 +0700

    profile.jsx: added comments

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 function Profile() {
   return (
     <div className="container mx-auto">
+      {/* Flexbox for profile pic and profile url */}
       <div className="flex gap-2 justify-start bg-blue-200">
         <div className="bg-blue-600">1</div>
         <div className="bg-blue-600">2</div>
