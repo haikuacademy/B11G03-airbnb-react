5bd6f6877c480421f2c98114a6bfea06057d3dbc
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-27 18:08:48 +0700

    HouseEdit grid, flex & container

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -1,7 +1,10 @@
 function HouseEdit() {
   return (
-    <div>
-      <p>Hello from House Edit!</p>
+    <div className="container mx-auto">
+      <div className="grid bg-pink-200 grid-cols-2 gap-24">
+        <div class="bg-pink-600">1</div>
+        <div class="bg-pink-600">2</div>
+      </div>
     </div>
   )
 }
