0ccaa13d9d8dde6354b736ddf44255aaabc7291b
I-bangash <irtezabangash3d@gmail.com>
2024-02-27 11:47:53 +0700

    routes: added login.jsx and imported to index.js

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Houses from './components/Houses'
 import House from './components/House'
+import Login from './components/Login'
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Houses />} />
-                <Route path='/houses/:id' element={<House />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Houses />} />
+        <Route path='/houses/:id' element={<House />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default Router
