de88936a4f093d82dd10014f23f55cafdec45640
I-bangash <irtezabangash3d@gmail.com>
2024-02-27 14:28:06 +0700

    profile.jsx: added flex for profile pic and url

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,12 @@
 function Profile() {
-    return (
-        <div>
-            <p>
-                Hello from Profile
-            </p>
-        </div>
-    )
+  return (
+    <div className="container mx-auto">
+      <div className="flex gap-2 justify-start bg-blue-200">
+        <div className="bg-blue-600">1</div>
+        <div className="bg-blue-600">2</div>
+      </div>
+    </div>
+  )
 }
 
 export default Profile
