4b79120acecc61e57510e0d540b87c03dacc2ca0
I-bangash <irtezabangash3d@gmail.com>
2024-02-28 12:31:52 +0700

    nav.jsx: added nav component and imported it to all relevant routes

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,6 +1,9 @@
+import Nav from './Nav'
+
 function Listings() {
   return (
     <div className="container mx-auto">
+      <Nav />
       {/* Flexbox for new listings */}
       <div className="grid grid-cols-2 gap-20 bg-pink-200">
         <div className="bg-pink-600">1</div>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+function Nav() {
+  return (
+    <div>
+      <p>Hello from Nav</p>
+    </div>
+  )
+}
+export default Nav
