8d1a955219bdb97332ebb368bf4e8c9e767c6969
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-28 11:49:03 +0700

    Correcting the Filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,5 +1,14 @@
 function Filters() {
-  return <div class="bg-blue-600">1</div>
+  return (
+    <div className="flex bg-blue-200 justify-between">
+      <div class="bg-blue-600">1</div>
+      <div class="bg-blue-600">1</div>
+      <div class="bg-blue-600">1</div>
+      <div class="bg-blue-600">1</div>
+      <div class="bg-blue-600">1</div>
+      <div class="bg-blue-600">1</div>
+    </div>
+  )
 }
 
 export default Filters
--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -5,15 +5,8 @@ function Houses() {
   return (
     /* Adding container to position the content wrap the page */
     <div className="container mx-auto">
-      {/* Creating a flexbox for the search function in the page */}
-      <div className="flex bg-blue-200 justify-between">
-        <Filters />
-        <Filters />
-        <Filters />
-        <Filters />
-        <Filters />
-        <Filters />
-      </div>
+      {/* Importing flexbox for the search function from filters */}
+      <Filters />
       {/* Importing houses from HouseCard */}
       <div className="grid bg-pink-200 grid-cols-5 gap-3">
         <HouseCard />
