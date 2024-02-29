2284550cf5de2d10485715c4033680e0d011a066
I-bangash <irtezabangash3d@gmail.com>
2024-02-29 11:52:12 +0700

    housecard.jsx: added dummy text and styling

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,5 +1,17 @@
 function HouseCard() {
-  return <div class="bg-pink-600">1</div>
+  return (
+    <div className="p-3.5 border rounded-md">
+      <h5 className="font-bold">Pucket, Thailand</h5>
+      <span className="text-sm">2 rooms</span>
+      {' • '}
+      <span className="text-sm">2 bathrooms</span>
+      <h6 className="font-bold py-2">$120</h6>
+      <div className="flex justify-between">
+        <p>4.5</p>
+        <p>34</p>
+      </div>
+    </div>
+  )
 }
 
 export default HouseCard
--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -10,7 +10,7 @@ function Houses() {
       {/* Importing flexbox for the search function from filters */}
       <Filters />
       {/* Importing houses from HouseCard */}
-      <div className="grid bg-pink-200 grid-cols-5 gap-3">
+      <div className="grid grid-cols-5 gap-3">
         <HouseCard />
         <HouseCard />
         <HouseCard />
