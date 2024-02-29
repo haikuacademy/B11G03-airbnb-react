d41575d032938e6616ae3e067e366b368f9ff23c
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-29 13:59:31 +0700

    Adding a photo to guest review

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,27 +1,48 @@
 function Reviews() {
   return (
-    <div className="grid bg-pink-200 grid-cols-3 gap-24">
+    <div className="grid grid-cols-3 gap-24">
       <div className="col-span-2">
+        <p className="text-lg font-bold">34 Reviews</p>
+        <h1 className="text-sm">Overall ⭐️ Rating</h1>
         <Review />
         <Review />
         <Review />
       </div>
-      <div className="bg-pink-600 p-4"> form </div>
+      <div className="">
+        <div className="border-2 rounded p-2 m-2">
+          <h1 className="text-sm">Leave a Review</h1>
+          <h2 className="text-sm">⭐️ Rating</h2>
+        </div>
+      </div>
     </div>
   )
 }
 
 function Review() {
   return (
-    <div class="bg-pink-600 p-3">
-      <div class=" bg-blue-200">
-        <div className="flex bg-blue-200 gap-2">
-          {/* guest profile photo */}
-          <div class="bg-blue-600">5.1</div>
-          {/* guest name */}
-          <div class="bg-blue-600">5.2</div>
+    <div className="p-2 rounded border-2 m-2">
+      <div className="flex gap-2">
+        {/* guest profile photo */}
+        <div className="">
+          <img
+            src="https://randomuser.me/api/portraits/men/84.jpg"
+            alt="Guest review photo"
+            className="w-10 rounded-full"
+          />
+        </div>
+        {/* review date & guest name */}
+        <div>
+          <h1 className="text-xs text-slate-400">22 Jan 2024</h1>
+          <h2 className="text-sm font-semibold">Mike Lino</h2>
         </div>
       </div>
+      {/* review star rating & review */}
+      <h1 className="text-xs"> ⭐️ Rating</h1>
+      <h2 className="text-sm">
+        Great place to stay! The house is very clean and confortable, and the
+        location is perfect. The host was very friendly and helpful. Highly
+        recommend!
+      </h2>
     </div>
   )
 }
