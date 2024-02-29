57f20e0f3fc489d4650e26ebb568c23ee607335f
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-02-29 10:56:39 +0700

    Adjustments to Review and Reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,12 @@
 function Reviews() {
   return (
     <div className="grid bg-pink-200 grid-cols-3 gap-24">
-      <Review />
+      <div className="col-span-2">
+        <Review />
+        <Review />
+        <Review />
+      </div>
+      <div className="bg-pink-600 p-4"> form </div>
     </div>
   )
 }
@@ -9,15 +14,16 @@ function Reviews() {
 function Review() {
   return (
     <>
-      <div class="col-span-2 bg-blue-200">
-        <div className="flex bg-blue-200 gap-2">
-          {/* guest profile photo */}
-          <div class="bg-blue-600">5.1</div>
-          {/* guest name */}
-          <div class="bg-blue-600">5.2</div>
+      <div class="bg-pink-600 p-3">
+        <div class=" bg-blue-200">
+          <div className="flex bg-blue-200 gap-2">
+            {/* guest profile photo */}
+            <div class="bg-blue-600">5.1</div>
+            {/* guest name */}
+            <div class="bg-blue-600">5.2</div>
+          </div>
         </div>
       </div>
-      <div class="bg-pink-600">6</div>
     </>
   )
 }
