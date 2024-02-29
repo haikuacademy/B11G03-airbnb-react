67fa9156e0012cd6b017473726418b7149186faa
I-bangash <irtezabangash3d@gmail.com>
2024-02-29 12:24:41 +0700

    fix: changed image t radius to rounded

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,10 +1,10 @@
 function HouseCard() {
   return (
-    <div className="block border rounded-md overflow-hidden">
+    <div className="block border rounded-md">
       <img
         src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
-        alt="Description of the image"
-        size=""
+        alt="House image"
+        className="rounded-t"
       />
       <div className="p-3">
         <h5 className="font-bold">Phuket, Thailand</h5>
