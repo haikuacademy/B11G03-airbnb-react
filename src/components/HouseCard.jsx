c11940d5bad2919619f8478b41fa0ae31cecd09a
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-03-02 20:10:27 +0700

    house card link
    link to house/1

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,21 +1,25 @@
+import { Link } from 'react-router-dom'
+
 function HouseCard() {
   return (
-    <div className="block border rounded-md">
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
-        alt="House image"
-        className="rounded-t"
-      />
-      <div className="p-3">
-        <h5 className="font-bold">Phuket, Thailand</h5>
-        <span className="text-sm">2 rooms • 2 bathrooms</span>
-        <h6 className="font-bold py-2">$120</h6>
-        <div className="flex justify-between">
-          <p>4.5</p>
-          <p>34</p>
+    <Link to="/houses/1">
+      <div className="block border rounded-md">
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
+          alt="House image"
+          className="rounded-t"
+        />
+        <div className="p-3">
+          <h5 className="font-bold">Phuket, Thailand</h5>
+          <span className="text-sm">2 rooms • 2 bathrooms</span>
+          <h6 className="font-bold py-2">$120</h6>
+          <div className="flex justify-between">
+            <p>4.5</p>
+            <p>34</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
