f981c6252b46f959e3224bd130c19a41c49cc399
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-03-04 13:34:55 +0700

    Installing solid package

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function Reviews({ rating }) {
   const rev1 = {
