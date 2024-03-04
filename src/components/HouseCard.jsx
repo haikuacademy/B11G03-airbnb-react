9d5faea9e2086c9e0798fc33cd86137269e170a3
AlvesDv <146094336+AlvesDv@users.noreply.github.com>
2024-03-04 13:33:37 +0700

    Using solid package

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function HouseCard() {
   const rating = 5
