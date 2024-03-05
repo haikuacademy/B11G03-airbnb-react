29b2a0c79547ed43b6645f0ce71a52f1e9bba37a
I-bangash <irtezabangash3d@gmail.com>
2024-03-05 15:59:13 +0700

    filter.jsx: added icons to the filter component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,51 +1,58 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faBed,
+  faDollarSign,
+  faHome,
+  faHouse,
+  faSort,
+  faStar
+} from '@fortawesome/free-solid-svg-icons'
+
 function Filters() {
   return (
     <form>
       <div className="flex bg-slate-100 gap-2 h-14 p-2 justify-between rounded-md mb-3">
         {/* Location */}
-        <select
-          type="select"
-          class="rounded-md p-2 border-2 border-slate-200 w-[20%] text-sm"
-        >
-          <option value="">Any Location</option>
-          <option value="Thailand">Thailand</option>
-          <option value="Vietnam">Vietnam</option>
-        </select>
+        <div className="bg-white flex items-center rounded-md p-2 border-2 border-slate-200 w-[20%] gap-2 text-sm">
+          <FontAwesomeIcon icon={faHouse} className="" />
+          <select type="select" class="w-full">
+            <option value="">Any Location</option>
+            <option value="Thailand">Thailand</option>
+            <option value="Vietnam">Vietnam</option>
+          </select>
+        </div>
 
         {/* Number of rooms */}
-        <select
-          type="select"
-          class="rounded-md p-2 border-2 border-slate-200 w-[20%] text-sm"
-        >
-          <option value="">Any Rooms</option>
-          <option value="1">1 room</option>
-          <option value="2">2 rooms</option>
-        </select>
+        <div className="bg-white flex items-center rounded-md p-2 border-2 border-slate-200 w-[20%] gap-2 text-sm">
+          <FontAwesomeIcon icon={faBed} className="" />
+          <select type="select" class="w-full">
+            <option value="">Any Rooms</option>
+            <option value="1">1 room</option>
+            <option value="2">2 rooms</option>
+          </select>
+        </div>
 
         {/* Max price */}
-        <input
-          type="number"
-          class="rounded-md p-2 border-2 border-slate-200 w-[20%] text-sm"
-          placeholder="max price"
-        />
+        <div className="bg-white flex items-center rounded-md p-2 border-2 border-slate-200 w-[20%] gap-2 text-sm">
+          <FontAwesomeIcon icon={faDollarSign} className="" />
+          <input type="number" class="w-full" placeholder="max price" />
+        </div>
 
         {/* Sort by */}
-        <select
-          type="select"
-          class="rounded-md p-2 border-2 border-slate-200 w-[20%] text-sm"
-        >
-          <option value="">sort by</option>
-          <option value="price_lowest">Lowest Price</option>
-          <option value="price_highest">Highest Price</option>
-          <option value="highest_rated">Highest Rated</option>
-        </select>
+        <div className="bg-white flex items-center rounded-md p-2 border-2 border-slate-200 w-[20%] gap-2 text-sm">
+          <FontAwesomeIcon icon={faSort} className="" />
+          <select type="select" class="w-full">
+            <option value="">sort by</option>
+            <option value="price_lowest">Lowest Price</option>
+            <option value="price_highest">Highest Price</option>
+            <option value="highest_rated">Highest Rated</option>
+          </select>
+        </div>
 
         {/* Keywords */}
-        <input
-          type="text"
-          class="rounded-md p-2 border-2 border-slate-200 w-[20%] text-sm"
-          placeholder="keywords..."
-        />
+        <div className="bg-white flex items-center rounded-md p-2 border-2 border-slate-200 w-[20%] gap-2 text-sm">
+          <input type="text" class="w-full" placeholder="keywords..." />
+        </div>
 
         {/* Submit button */}
         <button className="rounded-md bg-[#FB7185] text-white p-2 ">
