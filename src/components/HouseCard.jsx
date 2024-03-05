5c68930b28f0dd5978c146616ee1b15a7890ea60
I-bangash <irtezabangash3d@gmail.com>
2024-03-05 10:56:57 +0700

    houses & houseCard.jsx: added props and dynamic data displaying in both files

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,18 +1,19 @@
-function HouseCard() {
+function HouseCard(props) {
+  const { location, rooms, bathrooms, price, rating, reviews, photo } =
+    props.house
+
   return (
     <div className="block border rounded-md">
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
-        alt="House image"
-        className="rounded-t"
-      />
+      <img src={photo} alt="House image" className="rounded-t" />
       <div className="p-3">
-        <h5 className="font-bold">Phuket, Thailand</h5>
-        <span className="text-sm">2 rooms • 2 bathrooms</span>
-        <h6 className="font-bold py-2">$120</h6>
+        <h5 className="font-bold">{location}</h5>
+        <span className="text-sm">
+          {rooms} rooms • {bathrooms} bathrooms
+        </span>
+        <h6 className="font-bold py-2">${price}</h6>
         <div className="flex justify-between">
-          <p>4.5</p>
-          <p>34</p>
+          <p>{rating}</p>
+          <p>{reviews}</p>
         </div>
       </div>
     </div>
