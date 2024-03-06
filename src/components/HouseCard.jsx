b20f9bd0a37664170101cce221898abcfddb4a3a
I-bangash <irtezabangash3d@gmail.com>
2024-03-06 13:59:59 +0700

    resolved merge conflicts

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -2,62 +2,66 @@ import { Link } from 'react-router-dom'
 
 function HouseCard(props) {
   const { isListing } = props
-  const { location, rooms, bathrooms, price, rating, reviews, photo } =
-    props.house
-  const { isbooking } = props.isbooking
+  const { isbooking } = props
+  const {
+    houseId,
+    location,
+    rooms,
+    bathrooms,
+    priceNight,
+    reviewRating,
+    totalReviews,
+    photoUrl,
+    checkIn,
+    checkOut,
+    totalNights,
+    totalPrice
+  } = props.house
+  console.log(props.house)
 
   return (
-    <Link to={`/houses/${house.houseId}`}>
+    <Link to={`/houses/${houseId}`}>
       <div className="block border rounded-md">
-        <img src={photo} alt="House image" className="rounded-t" />
+        <img src={photoUrl} alt="House image" className="rounded-t" />
         <div className="p-3">
           <h5 className="font-bold">{location}</h5>
           <span className="text-sm">
             {rooms} rooms • {bathrooms} bathrooms
           </span>
-          <h6 className="font-bold py-2">${price}</h6>
+          <h6 className="font-bold py-2">${priceNight}</h6>
           <div className="flex justify-between">
-            <p>{rating}</p>
-            <p>{reviews}</p>
-            <img src={house.photoUrl} alt="House image" className="rounded-t" />
-            <div className="p-3">
-              <h5 className="font-bold">{house.location}</h5>
-              <span className="text-sm">
-                {house.rooms} Bedrooms • {house.bathrooms} Bathrooms
-              </span>
-              <h6 className="font-bold py-2"> ${house.priceNight}</h6>
-              <div>
-                {house.reviewRating}(Rating) {house.totalReviews}(TotalReviews)
-              </div>
-              <div>
-                {isbooking ? (
-                  <div className="bg-green-200 border rounded-sm">
-                    {' '}
-                    {house.checkIn} {house.checkOut}
-                    <div>
-                      {house.totalNights} Nights = ${house.totalPrice}
-                    </div>
-                  </div>
-                ) : null}
-              </div>
-              {isListing && (
-                <div className="flex justify-start gap-2 mt-2">
-                  <button
-                    type="submit"
-                    className="rounded-md font-light border p-1 px-2 text-sm"
-                  >
-                    View
-                  </button>
-                  <button
-                    type="submit"
-                    className="rounded-md font-light border p-1 px-2 text-sm"
-                  >
-                    Edit
-                  </button>
-                </div>
-              )}
-            </div>
+            <p>{reviewRating}</p>
+            <p>{totalReviews}</p>
           </div>
+
+          {/* if is Listing page */}
+          {isListing && (
+            <div className="flex justify-start gap-2 mt-2">
+              <button
+                type="submit"
+                className="rounded-md font-light border p-1 px-2 text-sm"
+              >
+                View
+              </button>
+              <button
+                type="submit"
+                className="rounded-md font-light border p-1 px-2 text-sm"
+              >
+                Edit
+              </button>
+            </div>
+          )}
+
+          {/* if is booking page */}
+          {isbooking ? (
+            <div className="bg-green-200 border rounded-sm">
+              {' '}
+              {checkIn} {checkOut}
+              <div>
+                {totalNights} Nights = ${totalPrice}
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </Link>
