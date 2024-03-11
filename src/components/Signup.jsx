fcdef443e817d69dc20309b47b8e366bf9654a89
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-03-11 09:25:38 +0700

    signup form on progress

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,8 +3,15 @@ import { useState } from 'react'
 function Signup() {
   //state values
   const [validEmail, setValidEmail] = useState(true)
+  const [validPassword, setValidPassword] = useState(true)
 
-  //function
+  //prevent the form to be submited
+  const submitForm = async (event) => {
+    event.preventDefault()
+    console.log(event.target.email.value)
+    console.log(event.target.password.value)
+  }
+  //function to validate the email
   const validateEmail = (email) => {
     if ((email.includes('@') && email.includes('.')) || !email) {
       setValidEmail(true)
@@ -12,12 +19,19 @@ function Signup() {
       console.log('Email is not valid')
       setValidEmail(false)
     }
-    console.log(email)
+  }
+  //function to validate the password
+  const validatePassword = (password) => {
+    if (password.lenght < 6) {
+      setValidPassword(true)
+    } else {
+      setValidPassword(false)
+    }
   }
 
   return (
     <div className=" rounded-md w-80 mx-auto mt-16 border-2 p-4">
-      <form className="grid gap-2 w-full ">
+      <form className="grid gap-2 w-full " onSubmit={submitForm}>
         <span className="flex justify-center">
           <img
             src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png"
@@ -44,7 +58,15 @@ function Signup() {
 
         {/* Password */}
         <label className="text-slate-500 text-sm">Password</label>
-        <input className="border rounded-md h-10 px-2" type="password" />
+        {!validPassword && (
+          <span className=" text-red-700">Password too short</span>
+        )}
+        <input
+          className="border rounded-md h-10 px-2"
+          type="password"
+          name="password"
+          onChange={(e) => validatePassword(e.target.value)}
+        />
 
         {/* Profile Picture */}
         <label className="text-slate-500 text-sm">Profile Picture</label>
